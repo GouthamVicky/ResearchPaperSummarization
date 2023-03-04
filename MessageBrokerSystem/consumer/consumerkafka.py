c7b946d0d9f7578c255571c435f47dba2ba89223
GouthamVicky <gouthamvignesh@vakilsearch.com>
2023-03-04 14:11:31 +0000

    kafka broker setup

--- MessageBrokerSystem/consumer/consumerkafka.py
+++ MessageBrokerSystem/consumer/consumerkafka.py
@@ -3,25 +3,17 @@ import requests
 
 import numpy as np
 from confluent_kafka import Consumer,Producer
-from grobidparser import detectAbstractParagraph
 from generateSummaryandMetrics import summaryGenerator,summaryMetrics
 
 from sentencebreak import process_stanza
 
 
 # Set up Kafka consumer
-consumer = Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest'})
+consumer = Consumer({'bootstrap.servers':'localhost:9092','group.id':'python-consumer','auto.offset.reset':'earliest','message.max.bytes': 1000000})
 
 print('Available topics to consume: ', consumer.list_topics().topics)
 
 consumer.subscribe(['ContributeSentences'])
-# Set up Kafka producer
-producer = Producer({'bootstrap.servers':'localhost:9092'})
-
-
-
-# Set up the Grobid API endpoint
-grobid_url = 'http://localhost:8070/api/processFulltextDocument'
 
 
 def main():
@@ -29,20 +21,20 @@ def main():
     while True:
         message=consumer.poll(1.0)
         if message is None:
+            print('No message received')
             continue
         if message.error():
             print('Error: {}'.format(message.error()))
             continue
         # Extract PDF data from message
-        pdf_message = json.loads(message.value.decode('utf-8'))
-        pdf_data = pdf_message['data']
+        pdf_message = json.loads(message.value().decode('utf-8'))
+        
         pdf_filename = pdf_message['filename']
         
-        # Extract text from PDF using Grobid
-        headers = {'Content-Type': 'application/pdf'}
-        response = requests.post(grobid_url, headers=headers, data=pdf_data)
+        paragraphText=pdf_message['paragraphText']
+        abstract=pdf_message['abstract']
         
-        abstract,paragraphText=detectAbstractParagraph(response)
+        
         
         
         #Sentence processing using Stanza
@@ -57,9 +49,9 @@ def main():
         
         
         result_message = {'filename': pdf_filename, 'Abstract':abstract,'generatedSummary': summary, 'rouge_scores': rouge_scores}
-        producer.poll(1)
-        producer.produce('ContributeSentences', json.dumps(result_message).encode('utf-8'))
-        producer.flush()
+        
         
         # Print results to console 
         print(result_message)
+       
+    main()
--- MessageBrokerSystem/producer/producerkafka.py
+++ MessageBrokerSystem/producer/producerkafka.py
@@ -2,7 +2,10 @@ import os
 import json
 from confluent_kafka import Producer
 import logging
-
+import base64
+from grobidparser import detectAbstractParagraph
+import time
+import requests
 
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -14,19 +17,20 @@ logger.setLevel(logging.INFO)
 
 
 # Set up Kafka producer
-producer = Producer({'bootstrap.servers':'localhost:9092'})
+producer = Producer({'bootstrap.servers':'localhost:9092','message.max.bytes': 10485880 })
 
 def receipt(err,msg):
     if err is not None:
         print('Error: {}'.format(err))
     else:
-        message = 'Produced message on topic {} with value of {}\n'.format(msg.topic(), msg.value().decode('utf-8'))
+        message = 'Produced message on topic {} \n'.format(msg.topic())
         logger.info(message)
         print(message)
 
 #####################
 print('Kafka Producer has been initiated...')
-
+# Set up the Grobid API endpoint
+grobid_url = 'http://localhost:8070/api/processFulltextDocument'
 
 def main():
     
@@ -38,9 +42,15 @@ def main():
     for file in pdf_files:
         with open(os.path.join(pdf_dir, file), 'rb') as f:
             pdf_data = f.read()
-        message = {'filename': file, 'data': pdf_data}
+        
+        headers = {'Content-Type': 'application/pdf'}
+        response = requests.post(grobid_url, headers=headers, data=pdf_data)
+        abstract,paragraphText=detectAbstractParagraph(response)
+        
+        message = {'filename': file, 'abstract': abstract, "paragraphText": paragraphText}
+        
         producer.poll(1)
-        producer.produce('ContributeSentences', json.dumps(message).encode('utf-8'))
+        producer.produce('ContributeSentences', json.dumps(message).encode('utf-8'),callback=receipt)
         producer.flush()
 
 if __name__ == '__main__':
